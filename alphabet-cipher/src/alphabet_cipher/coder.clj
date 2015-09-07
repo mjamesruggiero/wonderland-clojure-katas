8497e71e733f34101030a9e8c7de5b1d1097d5c0
mjamesruggiero <mjamesruggiero@gmail.com>
2015-09-07 16:44:26 -0700

    establish secret zipping and lookup

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,5 +1,35 @@
 (ns alphabet-cipher.coder)
 
+(def alpha "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+(defn rotate [n]
+  (str (subs alpha n (count alpha)) (subs alpha 0 n)))
+
+(defn alpha-pos
+  "position of a letter in a seq" [letter seqnce]
+  (.indexOf seqnce letter))
+
+(defn row-column [row-letter col-letter]
+  [(alpha-pos row-letter alpha) (alpha-pos col-letter alpha)])
+
+(defn cipher-letter [row-letter col-letter]
+  (let [rc-pair (row-column row-letter col-letter)
+        row (first rc-pair)
+        column (second rc-pair)
+        rotated (subs (rotate row) column (+ column 1))]
+   rotated))
+
+(defn match-secret-to-string [secret string]
+  (let [partsubs (subs secret 0 (mod (count string) (count secret)))
+        repeated (apply str (repeat (quot (count string) (count secret)) secret))]
+    (apply str repeated partsubs)))
+
+(defn map-secret-and-string
+  [secret string]
+  (map str
+       (seq (clojure.string/upper-case string))
+       (seq (clojure.string/upper-case secret))))
+
 (defn encode [keyword message]
   "encodeme")
 
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,16 +2,44 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
-(deftest test-encode
-  (testing "can encode given a secret keyword"
-    (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
-           (encode "vigilance" "meetmeontuesdayeveningatseven")))
-    (is (= "egsgqwtahuiljgs"
-           (encode "scones" "meetmebythetree")))))
+(deftest test-rotate
+  (testing "can rotate the alpha sequence"
+    (is (= "BCDEFGHIJKLMNOPQRSTUVWXYZA" (rotate 1)))))
 
-(deftest test-decode
-  (testing "can decode an cyrpted message given a secret keyword"
-    (is (= "meetmeontuesdayeveningatseven"
-           (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
-    (is (= "meetmebythetree"
-           (decode "scones" "egsgqwtahuiljgs")))))
+(deftest test-alpha-pos
+  (testing "can find the zero-indexed element in the sequence"
+    (is (= 2 (alpha-pos "C" "ARCTIC")))))
+
+(deftest test-cipher-letter
+  (testing "with two letters you can find the corresponding cipher letter"
+    (is (= "P" (cipher-letter "O" "B")))))
+
+(deftest test-match-secret-to-string
+  (testing "secret is repeated whole or part to match string to be encoded"
+    (is (= "vietnamvietnamvie" (match-secret-to-string "vietnam" "smellslikevictory")))))
+
+(def expected-mapping
+  (seq ["SV" "MI" "EE" "LT" "LN" "SA" "LM" "IV" "KI"
+        "EE" "VT" "IN" "CA" "TM" "OV" "RI" "YE"]))
+
+(def repeating-secret
+  (match-secret-to-string "vietnam" "smellslikevictory"))
+
+(deftest test-map-secret-and-string
+  (testing "secret and string are put into pairs"
+    (is (= expected-mapping
+           (map-secret-and-string repeating-secret "smellslikevictory")))))
+
+;(deftest test-encode
+  ;(testing "can encode given a secret keyword"
+    ;(is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+           ;(encode "vigilance" "meetmeontuesdayeveningatseven")))
+    ;(is (= "egsgqwtahuiljgs"
+           ;(encode "scones" "meetmebythetree")))))
+
+;(deftest test-decode
+  ;(testing "can decode an cyrpted message given a secret keyword"
+    ;(is (= "meetmeontuesdayeveningatseven"
+           ;(decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
+    ;(is (= "meetmebythetree"
+           ;(decode "scones" "egsgqwtahuiljgs")))))
