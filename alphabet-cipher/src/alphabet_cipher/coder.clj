3c8e103b9485272902ed7e6fc674016dfe9d3d83
mjamesruggiero <mjamesruggiero@gmail.com>
2015-09-10 22:53:29 -0700

    working codec
    * need to refactor a little

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -19,31 +19,36 @@
         rotated (subs (rotate row) column (+ column 1))]
    rotated))
 
-(defn match-secret-to-string [secret string]
+(defn match-keyword-length-to-message [secret string]
   (let [partsubs (subs secret 0 (mod (count string) (count secret)))
         repeated (apply str (repeat (quot (count string) (count secret)) secret))]
     (apply str repeated partsubs)))
 
-(defn map-secret-and-string
+(defn keyword-message->pairs
   [secret string]
   (map #(conj [] %1 %2)
        (seq (clojure.string/upper-case string))
        (seq (clojure.string/upper-case secret))))
 
-(def rs
-  (match-secret-to-string "vietnam" "smellslikevictory"))
-
-(map-secret-and-string rs "smellslikevictory")
-
 (defn encode [keyword message]
-  (let [repeating-secret (match-secret-to-string keyword message)
-        letter-pairs (map-secret-and-string repeating-secret message)]
+  (let [repeating-secret (match-keyword-length-to-message keyword message)
+        letter-pairs (keyword-message->pairs repeating-secret message)]
     (clojure.string/lower-case (apply str (map #(cipher-letter %) letter-pairs)))))
 
-;(encode "vietnam" "smellslikevictory")
+(defn find-row [chr]
+  (let [row-number (alpha-pos chr alpha)]
+       (rotate row-number)))
 
-;(encode "vigilance" "meetmeontuesdayeveningatseven")
-;(encode "vietnam" "smellslikevictory")
+(defn decode-pair
+  "keyword char and message char unlock decoded char"
+  [[keyword-char message-char]]
+  (let [rotated-row (find-row keyword-char)
+        decode-pos (alpha-pos message-char rotated-row)]
+    (subs alpha decode-pos (+ 1 decode-pos))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (let [keyword-seq (match-keyword-length-to-message keyword message)
+        pairs (map #(conj [] %1 %2)
+                (seq (clojure.string/upper-case keyword-seq))
+                (seq (clojure.string/upper-case message)))]
+    (clojure.string/lower-case (apply str (map #(decode-pair %) pairs)))))
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -12,34 +12,36 @@
 
 (deftest test-cipher-letter
   (testing "with two letters you can find the corresponding cipher letter"
-    (is (= "P" (cipher-letter \O \B)))))
+    (is (= "P" (cipher-letter [\O \B])))))
 
-(deftest test-match-secret-to-string
+(deftest test-match-keyword-length-to-message
   (testing "secret is repeated whole or part to match string to be encoded"
-    (is (= "vietnamvietnamvie" (match-secret-to-string "vietnam" "smellslikevictory")))))
+    (is (= "vietnamvietnamvie" (match-keyword-length-to-message "vietnam" "smellslikevictory")))))
 
 (def expected-mapping
-  (seq ["SV" "MI" "EE" "LT" "LN" "SA" "LM" "IV" "KI"
-        "EE" "VT" "IN" "CA" "TM" "OV" "RI" "YE"]))
+  (seq [[ \S \V ] [ \M \I ] [ \E \E ] [ \L \T ]
+        [ \L \N ] [ \S \A ] [ \L \M ] [ \I \V ] [ \K \I ]
+        [ \E \E ] [ \V \T ] [ \I \N ] [ \C \A ] [ \T \M ]
+        [ \O \V ] [ \R \I ] [ \Y \E ]]))
 
-(def repeating-secret
-  (match-secret-to-string "vietnam" "smellslikevictory"))
+(def repeating
+  (match-keyword-length-to-message "vietnam" "smellslikevictory"))
 
-(deftest test-map-secret-and-string
+(deftest test-keyword-message->pairs
   (testing "secret and string are put into pairs"
     (is (= expected-mapping
-           (map-secret-and-string repeating-secret "smellslikevictory")))))
+           (keyword-message->pairs repeating "smellslikevictory")))))
 
-;(deftest test-encode
-  ;(testing "can encode given a secret keyword"
-    ;(is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
-           ;(encode "vigilance" "meetmeontuesdayeveningatseven")))
-    ;(is (= "egsgqwtahuiljgs"
-           ;(encode "scones" "meetmebythetree")))))
+(deftest test-encode
+  (testing "can encode given a secret keyword"
+    (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+           (encode "vigilance" "meetmeontuesdayeveningatseven")))
+    (is (= "egsgqwtahuiljgs"
+           (encode "scones" "meetmebythetree")))))
 
-;(deftest test-decode
-  ;(testing "can decode an cyrpted message given a secret keyword"
-    ;(is (= "meetmeontuesdayeveningatseven"
-           ;(decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
-    ;(is (= "meetmebythetree"
-           ;(decode "scones" "egsgqwtahuiljgs")))))
+(deftest test-decode
+  (testing "can decode an cyrpted message given a secret keyword"
+    (is (= "meetmeontuesdayeveningatseven"
+           (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
+    (is (= "meetmebythetree"
+           (decode "scones" "egsgqwtahuiljgs")))))
