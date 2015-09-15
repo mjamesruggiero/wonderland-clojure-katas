e74c07aa383b5a9226dd7b1f3b73b98ed4e9c3ea
mjamesruggiero <mjamesruggiero@gmail.com>
2015-09-14 22:44:46 -0700

    Refactor for readability

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -6,30 +6,23 @@
   (apply str (concat (drop n alpha) (take n alpha))))
 
 (defn alpha-pos
-  "position of a letter in a seq" [letter seqnce]
+  "position of a letter in a seq"
+  [letter seqnce]
   (.indexOf seqnce (int letter)))
 
-(defn row-column [row-letter col-letter]
-  [(alpha-pos row-letter alpha) (alpha-pos col-letter alpha)])
+(defn row-column [row-char col-char]
+  [(alpha-pos row-char alpha) (alpha-pos col-char alpha)])
 
-(defn cipher-letter [[row-letter col-letter]]
-  (let [[row column] (row-column row-letter col-letter)
-        rotated (subs (rotate row) column (+ column 1))]
-   rotated))
+(defn cipher-letter [[row-char col-char]]
+  (let [[row column] (row-column row-char col-char)]
+        (subs (rotate row) column (+ column 1))))
 
 (defn match-keyword-length-to-message [kw message]
     (apply str (take (count message) (apply concat (repeat kw)))))
 
 (defn keyword-message->pairs
   [secret string]
-  (map #(conj [] %1 %2)
-       (seq string)
-       (seq secret)))
-
-(defn encode [keyword message]
-  (let [lengthened-kw (match-keyword-length-to-message keyword message)
-        letter-pairs (keyword-message->pairs lengthened-kw message)]
-    (apply str (map #(cipher-letter %) letter-pairs))))
+  (map vector (seq string) (seq secret)))
 
 (defn find-row [chr]
   (let [row-number (alpha-pos chr alpha)]
@@ -40,11 +33,14 @@
   [[keyword-char message-char]]
   (let [rotated-row (find-row keyword-char)
         decode-pos (alpha-pos message-char rotated-row)]
-    (subs alpha decode-pos (+ 1 decode-pos))))
+    (subs alpha decode-pos (+ decode-pos 1))))
+
+(defn encode [keyword message]
+  (let [lengthened-kw (match-keyword-length-to-message keyword message)
+        letter-pairs (keyword-message->pairs lengthened-kw message)]
+    (apply str (map #(cipher-letter %) letter-pairs))))
 
 (defn decode [keyword message]
   (let [keyword-seq (match-keyword-length-to-message keyword message)
-        pairs (map #(conj [] %1 %2)
-                (seq keyword-seq)
-                (seq message))]
+        pairs (keyword-message->pairs message keyword-seq)]
     (apply str (map #(decode-pair %) pairs))))
